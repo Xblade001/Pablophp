6b26ba9568416b8d6f0bca11044ba507ef1cd3f2
usuario <usuario@usuario-HP>
2013-09-10 17:24:20 -0400

    Cambiada contraseña del dbphp a ""

--- db/conexion.php
+++ db/conexion.php
@@ -2,7 +2,7 @@
 
 @$dbhost = "localhost";  // indentificamos el Servidor donde nos conectamos
 @$dbusuario = "root"; // aqui debes ingresar el nombre de usuario del SGDB para acceder a la base de datos
-@$dbpassword = "Camaleon"; // password de acceso para el usuario con permisos sobre la base de datos
+@$dbpassword = ""; // password de acceso para el usuario con permisos sobre la base de datos
 @$db = "phpdb";        // Seleccionamos la base con la cual trabajar
 @$conn = mysqli_connect($dbhost, $dbusuario, $dbpassword, $db) or die("Error al conectarse a $db" . mysqli_error($conn));
 $link = $conn;
